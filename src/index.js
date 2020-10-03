faf67ff9d6afb7557d4695379ab14030b9f0673d
Fr3ud <oo.pavelko@gmail.com>
2020-10-03 10:25:17 +0300

    feat: add min

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!Array.isArray(array) || !array.length) return 0;
+
+  let min = array[0];
+  for (let value of array) {
+      if (value < min) {
+          min = value;
+      }
+  }
+
+  return min;
+};
 
 exports.max = function max (array) {
   return 0;
